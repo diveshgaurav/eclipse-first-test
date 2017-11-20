6112119ab93c41d42882906234e49e8748c08248
Divesh Gaurav <a>
2017-11-20 10:54:49 -0500

    Initial Commit

---
+++ TestGitProject/src/com/divesh/main/MainApp.java
@@ -0,0 +1,11 @@
+package com.divesh.main;
+
+import com.divesh.other.TestClass1;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		new TestClass1().testMethod();
+	}
+
+}
---
+++ TestGitProject/src/com/divesh/other/TestClass1.java
@@ -0,0 +1,8 @@
+package com.divesh.other;
+
+public class TestClass1 {
+
+	public void testMethod(){
+		System.out.println("testMethod called");
+	}
+}
