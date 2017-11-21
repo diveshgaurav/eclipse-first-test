912151e2299b2dfcc89b4a6f9d7b52dc5c06a956
divesh g <a>
2017-11-21 09:46:59 -0500

    divesh g <a>

--- TestGitProject/src/com/divesh/other/TestClass1.java
+++ TestGitProject/src/com/divesh/other/TestClass1.java
@@ -3,6 +3,6 @@ package com.divesh.other;
 public class TestClass1 {
 
 	public void testMethod(){
-		System.out.println("\" Test Method called \"");
+		System.out.println("\" Test Method called - \"");
 	}
 }
